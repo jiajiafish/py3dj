d49a6d1468cc98e85ceb88192a78563ed3ab849b
uglyfish <jiaxiaole_2009@163.com>
2018-08-13 20:39:39 +0800

    写了一个简单的前端

--- shopping/admin.py
+++ shopping/admin.py
@@ -7,10 +7,14 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['id','name','picture','category','price','kucun','onshelf','Createtime','Modtime']
     list_editable = ['category','price','kucun']
     list_per_page = 10
+    # save_as = True
+    list_filter = ['category']
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id','name','desc','picture']
+    list_editable = ['desc']
+
 # Register your models here.
 admin.site.register(Products,ProductAdmin)
 admin.site.register(Category)
