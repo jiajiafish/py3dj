7480a7318a593ce2fb46694af42808e05468d987
uglyfish <jiaxiaole_2009@163.com>
2018-08-13 14:23:22 +0800

    0813002

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class 商品类别表(models.Model):
+    商品名 = models.CharField(max_length=50,unique = True)
+    描述 = models.TextField()
+    图片 = models.ImageField(upload_to ="category",blank = True)
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
