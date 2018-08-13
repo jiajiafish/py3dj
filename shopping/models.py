10ac26d9d8fee7bb40249e5d0d2f0443c1c1cad0
uglyfish <jiaxiaole_2009@163.com>
2018-08-13 14:38:05 +0800

    增加了产品类别和产品列表

--- shopping/models.py
+++ shopping/models.py
@@ -2,14 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-class 商品类别表(models.Model):
-    商品名 = models.CharField(max_length=50,unique = True)
-    描述 = models.TextField()
-    图片 = models.ImageField(upload_to ="category",blank = True)
-    
-
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique = True)
+    desc = models.TextField()
+    picture = models.ImageField(upload_to ="category",blank = True)
+    class Meta:
+        verbose_name = "商品类别表"
+        db_table = "商品类别表"
     def __str__(self):
-        return 
+        return self.name
 
     def __unicode__(self):
-        return 
+        return self.name
+
+class products(models.Model):
+    name = models.CharField(max_length=100,unique = True)
+    desc = models.TextField()
+    picture = models.ImageField(upload_to ="category",blank = True)
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    price = models.DecimalField(max_digits=10,decimal_places=4)
+    kucun = models.IntegerField(default=0)
+    onshelf = models.BooleanField(default=True)
+    Createtime = models.DateTimeField(auto_now_add=True)
+    Modtime = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "商品列表"
+        db_table = "商品列表"
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
