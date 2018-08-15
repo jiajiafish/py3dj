2dae5b39aa37c01118d3ac70c4d7123f44ff9992
uglyfish <jiaxiaole_2009@163.com>
2018-08-15 18:22:31 +0800

    师范一下

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,10 @@ app_name = "home"
 
 urlpatterns = [
     path('1/', views.pg1, name="主页"),
-    # path('/2/', views.pg2, name="主页"),
-    # path('/3/', views.pg3, name="主页"),
-    # path('/4/', views.pg4, name="主页"),
-    # path('/5/', views.pg5, name="主页"),
+    path('/2/', views.pg2, name="主页"),
+    path('/3/', views.pg3, name="主页"),
+    path('/4/', views.pg4, name="主页"),
+    path('/5/', views.pg5, name="主页"),
 
     path('', views.homepage, name="主页"),
 ]
