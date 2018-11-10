436e2b500be33b9c0af328b9bd7a3458e653c036
jiaxiaole <jiaxiaole_2009@163.com>
2018-11-10 13:09:48 +0800

    准备开始魔改成ADL平台

--- home/models.py
+++ home/models.py
@@ -45,5 +45,21 @@ class Products(models.Model):
     def __unicode__(self):
         return self.name
 
+class Adl_type(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    desc = models.TextField(blank = True)
+    class Meta:
+        verbose_name = "ADL种类"
+        verbose_name_plural = verbose_name
+        db_table = "ADL种类"
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
 class AdlItem(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    adl_type = models.ForeignKey(Adl_type,on_delete=models.CASCADE)
+    adl_region = models.BooleanField()
+    
